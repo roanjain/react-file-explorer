92ebbc9f40abceed62cf62bfcc8ac351f9dc041a
Rohan Jain <roanjain@gmail.com>
2021-09-21 10:46:21 +0530

    cleaning up

--- src/api.js
+++
@@ -1,9 +0,0 @@
-const hostname = 'http://localhost:8000';
-
-const API_ = {
-    FILE_LIST : `${hostname}/fileExplorer/list?path=`,
-    FOLDER_CREATE: `${hostname}/fileExplorer/dir/create`,
-    DELETE: `${hostname}/fileExplorer/items/remove`,
-}
-
-export default API_
--- src/components/NoFile.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NoFilesFound = () => {
-    console.log("Rendering No File...")
-
-    return <div>No Files</div>
-}
-
-export default NoFilesFound
